5ca3ca7985b2713f4f9514e0cf2c657c76fe134f
johnitopaisah <johnitopaisah@gmail.com>
2023-06-25 15:32:59 +0100

    update with the print string function

--- handle_print.c
+++ handle_print.c
@@ -18,11 +18,10 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 	       
 	};
 /**
- * {'s', print_string}, {'%', print_percent},
  *		{'i', print_int}, {'d', print_int}, {'\0', NULL}
  *	};
  */
