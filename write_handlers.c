d3c3e9b8e698f49b47e408fdcbcfa2a83d9e1e98
johnitopaisah <johnitopaisah@gmail.com>
2023-06-25 15:56:08 +0100

    update with the print string function

--- write_handlers.c
+++ write_handlers.c
@@ -45,3 +45,51 @@ int handle_write_char(char c, char buffer[], int flags,
 	}
 	return (write(1, &buffer[0], 1));
 }
+
+/************************* WRITE NUMBER *************************/
+/**
+ * write_number - print a string
+ * @is_negative: Lists of argument
+ * @ind: char types.
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get the width
+ * @precision: precision specifier
+ * @size: Size specifier
+ */
+int write_number(int is_negative, int ind, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	int length = BUFF_SIZE - ind - 1;
+	char padd = ' ', extra_ch = 0;
+	
+	UNUSED(size);
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (is_negative)
+		extra_ch = '-';
+	else if (flags & F_PLUS)
+		extra_ch = '+';
+	else if (flags & F_SPACE)
+		extra_ch = ' ';
+
+	return (write_num(ind, buffer, flags,
+				width, precision, length, padd, extra_ch));
+}
+
+/************************ WRITE NUM *************************/
+/**
+ * write_num - Function that write number using buffer
+ * @ind: The index at which the number starts on the buffer
+ * @buffer: The buffer
+ * @flags: To Caculate the active flags
+ * @width: The width specifier
+ * @prec: Precision specifier
+ * @length: The number of length
+ * @padd: Padding character
+ * @extra_c: Extra character
+ *
+ * Return: The number of printed character
+ */
+
