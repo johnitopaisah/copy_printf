fefd797c2387c0cca7ba452ed5f979afa9aca07a
johnitopaisah <johnitopaisah@gmail.com>
2023-06-28 09:24:45 +0100

    fix the betty coding style for main.c function

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
-	
+
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
@@ -40,6 +40,6 @@ int main(void)
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
 	/* printf("Unknown:[%r]\n"); */
-	
+
 	return (0);
 }
