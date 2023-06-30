75f64a02d5b561fc88a803e892829d6ef1b8dfd8
johnitopaisah <johnitopaisah@gmail.com>
2023-06-30 22:34:24 +0100

    fix betty for line 118 "over 80 character"

--- write_handlers.c
+++ write_handlers.c
@@ -115,7 +115,8 @@ int write_num(int ind, char buffer[],
 		for (i = 1; i < width - length + 1; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-		if (flags & F_MINUS && padd == ' ')/*Assign extra character to left of buffer */
+		/*Assign extra character to left of buffer */
+		if (flags & F_MINUS && padd == ' ')
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
