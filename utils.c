09742cf0109783a3fde999f080158008b85dd21f
johnitopaisah <johnitopaisah@gmail.com>
2023-06-30 22:18:44 +0100

    updated the utils file with convert_size_number

--- utils.c
+++ utils.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+/**************************** is_digit Function ******************************/
 /**
  * is_digit - Function that virifies if na char is a digit
  * @c: The character to evaluate
@@ -12,4 +12,22 @@ int is_digit(char c)
 		return (1);
 
 	return (0);
+}
+
+/************************* convert_size_number **************************/
+/**
+ * convert_size_number - Function that casts a number to the specified size
+ * @num: The number to be casted
+ * @size: The number indicating the type to be casted
+ *
+ * Return: Return the casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
 }
