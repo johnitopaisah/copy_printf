bfcfdd1de003bd57501942b286935e5d47d2a8ba
johnitopaisah <johnitopaisah@gmail.com>
2023-07-01 16:43:20 +0100

    updates the function2.c file with print_hexa_upper function

--- function2.c
+++ function2.c
@@ -143,11 +143,11 @@ int print_hexadecimal(va_list types, char buffer[],
 				flags, width, precision, size));
 }
 
-/******************* PRINT HEX NUM IN LOWER OR UPPER ******************/
+/******************* PRINT HEX NUMBER IN UPPER HEXADECIMAL ******************/
 /**
- * print_hexa - Function that prints a hexadecimal number in lower or upper
+ * print_hexa_upper - Function that prints an unsigned number in upper
+ * hexadecimal notation
  * @types: The list of argument
- * @map_to: The array of value to map the number to
  * @buffer: The buffer of array to handle print
  * @flags: To calculate the active flags
  * @width: The width specifier
@@ -156,35 +156,9 @@ int print_hexadecimal(va_list types, char buffer[],
  *
  * Return: Return the number of characters printed
  */
-int print_hexa(va_list types, char map_to[], char buffer[],
+int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
-	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
-
-	UNUSED(width);
-
-	num = convert_size_unsignd(num, size);
-
-	if (num == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-
-	while (num > 0)
-	{
-		buffer[i--] = map_to[num % 16];
-		num /= 16;
-	}
-
-	if (flags &  F_HASH && init_num != 0)
-	{
-		buffer[i--] = flag_ch;
-		buffer[i--] = '0';
-	}
-
-	i++;
-
-	return (write_unsignd(0, i, buffer, flags, width, precision, size));
+	return (print_hexa(types, "0123456789ABCDEF", buffer,
+				flags, 'X', width, precision, size));
 }
