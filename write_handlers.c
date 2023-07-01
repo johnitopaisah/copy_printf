6f84f37bbf548f7e1b17f2975ec406994a3e006d
johnitopaisah <johnitopaisah@gmail.com>
2023-07-02 00:06:49 +0100

    update the write_handlers.c file with write_unsignd function

--- write_handlers.c
+++ write_handlers.c
@@ -134,3 +134,60 @@ int write_num(int ind, char buffer[],
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
 }
+
+/**************************** WRITE UNSIGND *****************************/
+/**
+ * write_unsignd - Function that writes an unsigned number
+ * @is_negative: The number indicating if the num is negative
+ * @ind: The index at which the number starts in the buffer
+ * @buffer: The buffer of array of characters
+ * @flags: To calculate the active flags
+ * @width: The width specifier
+ * @precision: The precision specifier
+ * @size: The size specifier
+ *
+ * Return: Return the number of characters written
+ */
+int write_unsignd(int is_negative, int ind, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	/* The number of stored at the buffer's right and start at position i */
+	int length = BUFF_SIZE - ind - 1, i = 0;
+	char padd = ' ';
+
+	UNUSED(is_negative);
+	UNUSED(size);
+
+	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
+		return (0); /* printf(".0d", 0) no character is printed */
+
+	if (precision > 0 && precision < length)
+		padd = ' ';
+
+	while (precision > length)
+	{
+		buffer[--ind] = '0';
+		length++;
+	}
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+
+	if (width > length)
+	{
+		for (i = 0; i < width - length; i++)
+			buffer[i] = padd;
+
+		buffer[i] = '\0';
+
+		if (flags & F_MINUS) /* Assign extra char to the left of buff*/
+			return (write(1, &buffer[ind], length) +
+					write(1, &buffer[0], i));
+		else/* Asign extra char to left of padding [padd>buffer]*/
+			return (write(1, &buffer[0], i) +
+					write(1, &buffer[ind], length));
+	}
+
+	return (write(1, &buffer[ind], length));
+}
+
