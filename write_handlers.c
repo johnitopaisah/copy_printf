39b86072f4e91c322693e6fc6f45a72c4ed9dc29
johnitopaisah <johnitopaisah@gmail.com>
2023-07-02 00:35:29 +0100

    update the write_handlers.c file with write_unsignd function

--- write_handlers.c
+++ write_handlers.c
@@ -191,3 +191,61 @@ int write_unsignd(int is_negative, int ind, char buffer[],
 	return (write(1, &buffer[ind], length));
 }
 
+/**************************** write pointer ******************************/
+/**
+ * write_pointer - Function that write a memory address
+ * @buffer: Arrays of characters
+ * @ind: Index at which the number starts in the buffer
+ * @length: The length of number
+ * @width: The width specifier
+ * @flags: The flags specifier
+ * @padd: The character representing the padding
+ * @extra_c: The charactar representing the extra char
+ * @padd_start: Index at which padding should start
+ *
+ * Return: Return the number of characters written
+ */
+int write_pointer(char buffer[], int ind, int length,
+		int width, int flags, char padd, char extra_c, int padd_start)
+{
+	int i;
+
+	if (width > length)
+	{
+		for (i = 3; i < width - length + 3; i++)
+			buffer[i] = padd;
+		buffer[i] = '\0';
+		if (flags & F_MINUS && padd == ' ')
+		{
+			buffer[--ind] = 'x';
+			buffer[--ind] = '0';
+			if (extra_c)
+				buffer[--ind] = extra_c;
+			return (write(1, &buffer[ind], length) +
+					write(1, &buffer[3], i - 3));
+		}
+		else if (!(flags & F_MINUS) && padd == ' ')
+		{
+			buffer[--ind] = 'x';
+			buffer[--ind] = '0';
+			if (extra_c)
+				buffer[--ind] = extra_c;
+			return (write(1, &buffer[3], i - 3) +
+					write(1, &buffer[ind], length));
+		}
+		else if (!(flags & F_MINUS) && padd == '0')
+		{
+			if (extra_c)
+				buffer[--padd_start] = extra_c;
+			buffer[1] = '0';
+			buffer[2] = 'x';
+			return (write(1, &buffer[padd_start], i - padd_start) +
+					write(1, &buffer[ind], length - (1 - padd_start) - 2));
+		}
+	}
+	buffer[--ind] = 'x';
+	buffer[--ind] = '0';
+	if (extra_c)
+		buffer[--ind] = extra_c;
+	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
+}
