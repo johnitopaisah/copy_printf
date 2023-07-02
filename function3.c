2ab0b5dc53d5ec35464fcf0a26105d39ce970d7e
johnitopaisah <johnitopaisah@gmail.com>
2023-07-02 14:23:27 +0100

    update function3.c file with print_non_printable

--- function3.c
+++ function3.c
@@ -130,5 +130,13 @@ int print_non_printable(va_list types, char buffer[],
 	{
 		if (is_printable(str[i]))
 			buffer[i + offset] = str[i];
+		else
+			offset += append_hexa_code(str[i], buffer, i + offset);
+
+		i++;
 	}
+
+	buffer[i + offset] = '\0';
+
+	return (write(1, buffer, i + offset));
 }
